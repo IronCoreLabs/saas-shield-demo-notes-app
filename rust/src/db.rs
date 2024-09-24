1b73c4a8705dfdf6390977d5dc0c2af221c78b9c
Colt Frederickson <coltfred@gmail.com>
2024-09-24 16:00:36 -0600

    Added migrations and db on disk.

--- rust/src/db.rs
+++ rust/src/db.rs
@@ -11,55 +11,8 @@ pub struct NoteTable {
     pub body: String,
     pub category: String,
     pub edek: String,
-    pub created: u64,
-    pub updated: u64,
-}
-
-pub async fn create_sqlite_tables(pool: &SqlitePool) -> std::result::Result<(), sqlx::Error> {
-    let mut conn = pool.acquire().await?;
-
-    sqlx::query(
-        "CREATE TABLE organization (
-      id          INTEGER PRIMARY KEY AUTOINCREMENT,
-      login       TEXT NOT NULL,
-      name        TEXT NOT NULL,
-      password    TEXT NOT NULL,
-      last_login  DATETIME DEFAULT current_timestamp,
-      created     DATETIME DEFAULT current_timestamp,
-      updated     DATETIME DEFAULT current_timestamp
-      )",
-    )
-    .execute(&mut *conn)
-    .await?;
-
-    sqlx::query(
-        "CREATE TABLE note (
-        id       INTEGER PRIMARY KEY AUTOINCREMENT,
-        org_id   INTEGER NOT NULL,
-        title    TEXT NOT NULL,
-        body     TEXT NOT NULL,
-        category TEXT NOT NULL,
-        edek     TEXT NOT NULL,
-        created  DATETIME DEFAULT current_timestamp,
-        updated  DATETIME DEFAULT current_timestamp,
-        FOREIGN KEY(org_id) REFERENCES organization(id)
-        )",
-    )
-    .execute(&mut *conn)
-    .await?;
-
-    sqlx::query(
-        "CREATE TABLE attachment (
-        id       INTEGER PRIMARY KEY AUTOINCREMENT,
-        note_id  INTEGER NOT NULL,
-        url      TEXT NOT NULL,
-        FOREIGN KEY(note_id) REFERENCES note(id)
-    )",
-    )
-    .execute(&mut *conn)
-    .await?;
-
-    Ok(())
+    pub created: String,
+    pub updated: String,
 }
 
 pub async fn create_note(
