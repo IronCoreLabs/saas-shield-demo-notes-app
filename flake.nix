32a563e3276f5b89c5814e1c0bee6a31aceae79e
Colt Frederickson <coltfred@gmail.com>
2024-09-24 14:07:09 -0600

    Add rust to flake and todo app for testing it works

--- flake.nix
+++ flake.nix
@@ -1,42 +1,61 @@
 {
   description = "SaaS Shield Demo Notes App";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      lib = import <nixpkgs/lib>;
-      pkgs = nixpkgs.legacyPackages.${system};
-      dependencies = with pkgs; [
-        nodejs_20
-        autoconf
-        mozjpeg
-        libtool
-        automake
-        nasm
-        libpng
-        optipng
-        pkg-config
-        gcc
-        dpkg
-        docker-compose
-        # (pkgs.yarn.override {nodejs = pkgs.nodejs-20_x;})
-        nodePackages.typescript
-        nodePackages.typescript-language-server
-        nodePackages.diagnostic-languageserver
-        nodePackages.eslint_d
-        (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
-        docker-credential-gcr
-        docker-credential-helpers
-      ];
-    in {
-      devShell = pkgs.mkShell {
-        shellHook = ''
-        '';
-        buildInputs = dependencies;
-      };
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        lib = import <nixpkgs/lib>;
+        pkgs = import nixpkgs { inherit system overlays; };
+        rusttoolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust/rust-toolchain.toml;
+        dependencies = with pkgs; [
+          nodejs_20
+          autoconf
+          mozjpeg
+          libtool
+          automake
+          nasm
+          libpng
+          optipng
+          pkg-config
+          gcc
+          dpkg
+          docker-compose
+          # (pkgs.yarn.override {nodejs = pkgs.nodejs-20_x;})
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+          nodePackages.diagnostic-languageserver
+          nodePackages.eslint_d
+          (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+          docker-credential-gcr
+          docker-credential-helpers
+        ];
+      in
+      rec {
+        devShell = pkgs.mkShell {
+          buildInputs =
+            with pkgs;
+            dependencies
+            ++ [
+              rusttoolchain
+              pkg-config
+              pkgs.libiconv
+              pkgs.prometheus
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ];
+        };
+
+      }
+    );
 }
